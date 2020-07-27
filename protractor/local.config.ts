54ce8c5b1678de0d55292b413b69bd21222129e0
Juan Manuel Imbachí G <juan.imbachi@correo.icesi.edu.co>
2020-07-26 21:09:56 -0500

    buy-tshirt test now works. protractor config files were modified

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -4,7 +4,7 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
+  getPageTimeout: 10000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
