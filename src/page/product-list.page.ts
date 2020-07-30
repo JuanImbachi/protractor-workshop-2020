d0bd097f740f40155598bec5ce20d379962cf827
Juan Manuel Imbachí G <juan.imbachi@correo.icesi.edu.co>
2020-07-30 10:18:03 -0500

    findByProduct & selectProduct methods were implemented

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,14 +1,30 @@
-import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions, ElementArrayFinder, $$ } from 'protractor';
 
 export class ProductListPage {
   private productList: ElementFinder;
-
+  private products: ElementArrayFinder;
   constructor () {
     this.productList = $('a.button.ajax_add_to_cart_button.btn.btn-default');
+    this.products = $$('product-container');
   }
 
   public async selectAProduct(): Promise<void> {
     await browser.wait(ExpectedConditions.elementToBeClickable(this.productList), 3000);
     await this.productList.click();
+  }
+
+  private findByProduct(productName: string) {
+
+    return this.products.filter(product =>
+      product.$('.product-name').getText().then((actualProduct) => {
+        return actualProduct === productName;
+      })
+    ).first();
+
+  }
+
+  public selectProduct (productName: string) {
+    const productSearched = this.findByProduct(productName);
+    productSearched.$('[itemprop="image"').click();
   }
 }
