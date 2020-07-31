02d102b02f359629463dfafdff41c5e8147b5bab
Juan Manuel Imbachí G <41926056+JuanImbachi@users.noreply.github.com>
2020-07-30 19:15:44 -0500

    findByProduct & selectProduct methods were implemented. (#20)
    * findByProduct & selectProduct methods were implemented
    
    * Empty lines deleted
    
    * selectProduct method, modified
    
    * selectProduct method, modified
    
    * SelectProduct method was modified
    
    * comment removed in productListPage

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,14 +1,23 @@
-import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
+import { browser, ExpectedConditions, ElementArrayFinder, $$ } from 'protractor';
 
 export class ProductListPage {
-  private productList: ElementFinder;
-
+  private products: ElementArrayFinder;
   constructor () {
-    this.productList = $('a.button.ajax_add_to_cart_button.btn.btn-default');
+    this.products = $$('.product-container');
   }
 
-  public async selectAProduct(): Promise<void> {
-    await browser.wait(ExpectedConditions.elementToBeClickable(this.productList), 3000);
-    await this.productList.click();
+  private findByProduct(productName: string) {
+    return this.products.filter(product =>
+      product.$('.product-name').getText().then((actualProduct) => {
+        return actualProduct === productName;
+      }
+      )
+    ).first();
+  }
+
+  public async selectProduct (productName: string) {
+    const productSearched = this.findByProduct(productName);
+    await browser.wait(ExpectedConditions.elementToBeClickable(productSearched), 3000);
+    await productSearched.$('.ajax_add_to_cart_button.btn.btn-default').click();
   }
 }
